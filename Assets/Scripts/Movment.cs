a08ffe62e5ee437b81453bc08ceeb4b69319d381
DamV2 <DamV2@MAQUETA>
2023-09-28 17:16:59 +0200

    new scene

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Movment : MonoBehaviour
 {
     private float horizontal;
-    private float speed;
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float jumpingPower = 8f;
     private bool isFacingRight = true;
+
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -16,6 +18,16 @@ public class Movment : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
 
         Flip();
     }
@@ -34,7 +46,7 @@ public class Movment : MonoBehaviour
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
-            localScale.x *= 1f;
+            localScale.x *= -1f;
             transform.localScale = localScale;
         }
     }
