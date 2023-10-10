ead6d115ee33f80f10563377b1f423ffe252051f
DamV2 <DamV2@MAQUETA>
2023-10-10 18:21:20 +0200

    trying flip detection

--- Assets/Scripts/CharacterMovment.cs
+++ Assets/Scripts/CharacterMovment.cs
@@ -10,6 +10,7 @@ public class CharacterMovment : MonoBehaviour
 
     public float moveInput;
     private bool facingRight = true;
+    private bool facingUp = false;
     private bool top = true;
 
     private Rigidbody2D rb;
@@ -20,16 +21,23 @@ public class CharacterMovment : MonoBehaviour
     public LayerMask whatIsGround;
 
 
-    private int flips;
-
     private int extraJumps;
     [SerializeField] private int extraJumpsValue = 2;
+
+
+    // trying Flip detection
+
+    private Quaternion initialRotation;
+    private bool hasFlipped = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         extraJumps = extraJumpsValue;
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
+
+        initialRotation = rb.transform.rotation;
 
     }
 
@@ -44,12 +52,12 @@ public class CharacterMovment : MonoBehaviour
         if (facingRight == false && moveInput > 0)
         {
             Debug.Log("Derecha: " + facingRight);
-            Flip();
+            FlipHorizontally();
         }
         if (facingRight ==  true && moveInput < 0)
         {
             Debug.Log("Derecha: " + facingRight);
-            Flip();
+            FlipHorizontally();
         }
 
     }
@@ -57,6 +65,8 @@ public class CharacterMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(transform.eulerAngles.z.ToString());
+
         if (isGrounded == true)
         {
             extraJumps = extraJumpsValue;
@@ -82,48 +92,47 @@ public class CharacterMovment : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Space: " + Input.GetKeyDown(KeyCode.Space));
             rb.gravityScale *= -1;
-            Rotation();
+            FlipVertically();
         }
 
+        // Check if the player's object has completed a full 360-degree rotation around the z-axis
+        if (transform.eulerAngles.z <= -360 || transform.eulerAngles.z >= 360)
+        {
+            Debug.Log("Player has completed a 360-degree flip!");
+        }
+        //    if (!hasFlipped)
+        //    {
+        //        // The player has completed a 360-degree flip, run your script here
+        //        Debug.Log("Player has completed a 360-degree flip!");
+
+        //        // You can add your script logic here
+
+        //        // Set hasFlipped to true to prevent the script from running repeatedly
+        //        hasFlipped = true;
+        //    }
+        //}
+        //else
+        //{
+        //    // Reset hasFlipped if the rotation is not greater than or equal to 360 degrees
+        //    hasFlipped = false;
+        //}
     }
 
-    void Flip()
+    void FlipHorizontally()
     {
         facingRight = !facingRight;
-
-        // Entender como funciona
         Vector3 Scaler = rb.transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
 
     }
-    private void Rotation()
+    void FlipVertically()
     {
-        if (!top)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 180f);
-        }
-        else
-        {
-            transform.eulerAngles = Vector3.zero;
-        }
-
-        //facingRight = !facingRight;
-        top = !top;
-    }
-
-    private void CheckFlips()
-    {
-        if (transform.rotation.z == 360)
-        {
-            Debug.Log("Flip!");
-        }
-    }
-
-    private void CheckIsGrounded()
-    {
+        facingUp = !facingUp;
+        Vector3 Scaler = rb.transform.localScale;
+        Scaler.y *= -1;
+        transform.localScale = Scaler;
 
     }
 }
